8b663422b2442190c034af68a112383f452c05d0
Md. Arif Hossain <kingkobra28112000@gmail.com>
2020-06-05 19:01:56 +0600

    Initial commit

---
+++ app/src/main/java/com/eshikhon/project/covid19/activities/SplashScreenActivity.java
@@ -0,0 +1,38 @@
+package com.eshikhon.project.covid19.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.eshikhon.project.covid19.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 2000;//ms
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature( Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+
+        setContentView(R.layout.activity_splash_screen);
+
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent( SplashScreenActivity.this, MainActivity.class);
+                SplashScreenActivity.this.startActivity(mainIntent);
+                SplashScreenActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
